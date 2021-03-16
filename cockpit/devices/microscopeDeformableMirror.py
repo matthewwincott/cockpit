ba6e7d4d05878d6daf71ebf16806833f0980e4af
Ian Dobbie <ian.dobbie@bioch.ox.ac.uk>
2021-03-16 18:15:18 +0000

    Fixed title of system flat parameters window and shorted sensorless params one

--- cockpit/devices/microscopeDeformableMirror.py
+++ cockpit/devices/microscopeDeformableMirror.py
@@ -134,7 +134,7 @@ class MicroscopeDeformableMirror(MicroscopeBase, device.Device):
     def set_sys_flat_param(self):
         inputs = cockpit.gui.dialogs.getNumberDialog.getManyNumbersFromUser(
                 None,
-                'Set the parameters for Sensorless Adaptive Optics routine',
+                'Set system flat parameters',
                 ['Number of iterations',
                  'Error threshold',
                  'System Flat Noll indeces'],
@@ -152,7 +152,7 @@ class MicroscopeDeformableMirror(MicroscopeBase, device.Device):
     def set_sensorless_param(self):
         inputs = cockpit.gui.dialogs.getNumberDialog.getManyNumbersFromUser(
                 None,
-                'Set the parameters for Sensorless Adaptive Optics routine',
+                'Set sensorless AO parameters',
                 ['Aberration range minima',
                  'Aberration range maxima',
                  'Number of measurements',
